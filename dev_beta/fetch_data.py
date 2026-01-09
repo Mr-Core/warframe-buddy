1678db8d86cacf27ba4ce2406e371708b3821341
Ivan Stanec <36860980+Mr-Core@users.noreply.github.com>
2026-01-09 15:47:47 +0100

    Add check/create for data folder

--- dev_beta/fetch_data.py
+++ dev_beta/fetch_data.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from config import FETCH_URL, HTML_FILE
+from pathlib import Path
 
 def fetch_data():
     """Fetch latest Warframe drop data"""
@@ -10,6 +11,8 @@ def fetch_data():
     response.encoding = 'utf-8'
 
     soup = BeautifulSoup(response.text, 'html.parser')
+    
+    Path(HTML_FILE).parent.mkdir(parents=True, exist_ok=True)
 
     with open(HTML_FILE, 'w', encoding='utf-8') as f:
         f.write(soup.prettify())
